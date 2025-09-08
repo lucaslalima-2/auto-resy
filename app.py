6f4ea5bb9d131df38eec16ac039e3196bd4c6f3f
Lucas LaLima <luke@macbookpro.mynetworksettings.com>
2025-09-07 22:17:51 -0400

    Working towards logging into RESY

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
+# Libaries
 from flask import Flask, jsonify
 import os
 from dotenv import load_dotenv
 
+# Functions
+from src.run_resy_bot import run_resy_bot
+
 # Load environment variables
 load_dotenv()
 
+# Store Flask app
 app = Flask(__name__)
 
+# Homepage
 @app.route("/")
 def home():
 	return "Resy Bot is running!"
 
+# GET Commands
 @app.route("/run-bot")
 def run_bot():
 # Placeholder for your bot logic
@@ -19,11 +26,6 @@ def run_bot():
 		return jsonify({"status": "success", "details": result})
 	except Exception as e:
 		return jsonify({"status": "error", "message": str(e)}), 500
-
-def run_resy_bot():
-	# This is where your scraping or automation logic goes
-	# For now, just return a dummy response
-	return "Bot executed successfully."
 
 if __name__ == "__main__":
   app.run(debug=True)
